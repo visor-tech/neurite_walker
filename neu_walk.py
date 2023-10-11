abfce4d1e31de0c96f0676799dd1aba5a1f7d51c
Eddy Xiao <bewantbe@gmail.com>
2023-10-11 15:35:17 +0800

    Add test code for indexing ording.

--- neu_walk.py
+++ neu_walk.py
@@ -1,12 +1,17 @@
 #!/usr/bin/env python3
 # add neu3dviewer to the path, we need some helper function in it
 
+# tips in ipython
+#%reload_ext autoreload
+#%autoreload 2
+
 import numpy as np
 from numpy.linalg import norm
 import tifffile
 import SimpleITK as sitk
 import zarr
 import matplotlib.pyplot as plt
+from matplotlib.pyplot import xlabel, ylabel, title, figure
 
 import sys
 sys.path.append('/home/xyy/code/py/vtk_test/')
@@ -17,6 +22,7 @@ from neu3dviewer.data_loader import (
 
 _a  = lambda x: np.array(x, dtype=np.float64)
 _ai = lambda x: np.array(x, dtype=int)
+imgshow = lambda im: plt.imshow(im, cmap='gray', origin='lower')
 
 def NormalSlice3DImage(img3d, p_center, vec_normal, vec_up):
     """
@@ -60,48 +66,148 @@ def NormalSlice3DImage(img3d, p_center, vec_normal, vec_up):
     return img_normal
 
 def ShowThreeViews(img3d, p_center):
-    plt.figure(9)
+    figure(9)
     # fixed z, facing -z, draw x-y plane (horizontal, vertical)
-    plt.imshow(img3d[int(p_center[2]), :, :], cmap='gray', origin='lower')
-    plt.xlabel('x')
-    plt.ylabel('y')
-    plt.title(f"z = {int(p_center[2])}")
+    imgshow(img3d[int(p_center[2]), :, :])
+    xlabel('x')
+    ylabel('y')
+    title(f"z = {int(p_center[2])}")
 
-    plt.figure(8)
+    figure(8)
     # x fixed, facing -x, draw y-z plane
-    plt.imshow(img3d[:, :, int(p_center[0])], cmap='gray', origin='lower')
-    plt.xlabel('y')
-    plt.ylabel('z')
-    plt.title(f"x = {int(p_center[0])}")
+    imgshow(img3d[:, :, int(p_center[0])])
+    xlabel('y')
+    ylabel('z')
+    title(f"x = {int(p_center[0])}")
 
-    plt.figure(7)
+    figure(7)
     # y fixed, facing y, draw x-z plane
-    plt.imshow(img3d[:, int(p_center[1]), :], cmap='gray', origin='lower')
-    plt.xlabel('x')
-    plt.ylabel('z')
-    plt.title(f"y = {int(p_center[1])}")
+    imgshow(img3d[:, int(p_center[1]), :])
+    xlabel('x')
+    ylabel('z')
+    title(f"y = {int(p_center[1])}")
 
 def ShowThreeViewsMIP(img3d):
-    plt.figure(9)
+    figure(9)
     # facing -z, draw x-y plane (horizontal, vertical)
-    plt.imshow(img3d.max(axis=0), cmap='gray', origin='lower')
-    plt.xlabel('x')
-    plt.ylabel('y')
-    plt.title("MIP")
+    imgshow(img3d.max(axis=0))
+    xlabel('x')
+    ylabel('y')
+    title("MIP")
 
-    plt.figure(8)
+    figure(8)
     # facing -x, draw y-z plane
-    plt.imshow(img3d.max(axis=2), cmap='gray', origin='lower')
-    plt.xlabel('y')
-    plt.ylabel('z')
-    plt.title("MIP")
+    imgshow(img3d.max(axis=2))
+    xlabel('y')
+    ylabel('z')
+    title("MIP")
 
-    plt.figure(7)
+    figure(7)
     # facing y, draw x-z plane
-    plt.imshow(img3d.max(axis=1), cmap='gray', origin='lower')
-    plt.xlabel('x')
-    plt.ylabel('z')
-    plt.title("MIP")
+    imgshow(img3d.max(axis=1))
+    xlabel('x')
+    ylabel('z')
+    title("MIP")
+
+def ExamBigImageContinuity():
+    p0 = _a([52785, 28145.6, 55668.9])
+    p_corner = np.floor(p0 / 128) * 128
+    img_block_path = '/mnt/xiaoyy/dataset/zarrblock'
+    imgz = zarr.open(img_block_path, mode='r')
+    idf_pb = lambda p, b: (slice(int(p[i]), int(p[i] + b[i]))
+                            for i in range(3))
+    block_size = _a([128, 128, 128])
+
+    f_l_gamma = lambda a, g: np.uint16(((np.float64(a) - a.min()) / (a.max()-a.min())) **(1/g) * (a.max()-a.min()) + a.min())
+
+    ## test direction of imshow
+    # consider (z,y,x)
+    figure(101)
+    img_t = np.zeros((64, 64), dtype=np.uint16)
+    img_t[0:10, 0:3] = 100   # (y,x)
+    imgshow(img_t)
+    xlabel('x')
+    ylabel('y')
+
+    # consider (x,y,z)
+    figure(102)
+    img_t = np.zeros((64, 64), dtype=np.uint16)
+    img_t[0:10, 0:3] = 100   # (x,y)
+    imgshow(img_t.T)
+    xlabel('x')
+    ylabel('y')
+    
+    ## test zarr
+
+    # consider (x,y,z)
+    img3d000 = imgz[*idf_pb(p_corner + _a([0,   0,   0]), block_size)]
+    img3d010 = imgz[*idf_pb(p_corner + _a([0, 128,   0]), block_size)]
+    img3d001 = imgz[*idf_pb(p_corner + _a([0,   0, 128]), block_size)]
+    img3d011 = imgz[*idf_pb(p_corner + _a([0, 128, 128]), block_size)]
+
+    figure(112)
+    imgshow(img3d000.max(axis=0).T)
+    xlabel('y')
+    ylabel('z')
+
+    # consider (z,y,x)
+    img3d000 = imgz[*idf_pb(p_corner + _a([0,   0,   0]), block_size)]
+    img3d010 = imgz[*idf_pb(p_corner + _a([0, 128,   0]), block_size)]
+    img3d001 = imgz[*idf_pb(p_corner + _a([0,   0, 128]), block_size)]
+    img3d011 = imgz[*idf_pb(p_corner + _a([0, 128, 128]), block_size)]
+
+    # MIP along x
+    figure(130)
+    imgshow(f_l_gamma(img3d000.max(axis=2), 3.0))
+    xlabel('y')
+    ylabel('z')
+    title('000')
+
+    figure(131)
+    imgshow(f_l_gamma(img3d001.max(axis=2), 3.0))
+    xlabel('y')
+    ylabel('z')
+    title('001')
+
+    figure(132)
+    imgshow(f_l_gamma(img3d010.max(axis=2), 3.0))
+    xlabel('y')
+    ylabel('z')
+    title('010')
+
+    figure(133)
+    imgshow(f_l_gamma(img3d011.max(axis=2), 3.0))
+    xlabel('y')
+    ylabel('z')
+    title('011')
+
+    # trying the plan that transpose the local array
+    # convert to (x,y,z) order
+    img3dfull = np.zeros((128,256,256), dtype=np.uint16)
+    img3dfull[*idf_pb(_a([0,   0,   0]), block_size)] = img3d000.transpose()
+    img3dfull[*idf_pb(_a([0,   0, 128]), block_size)] = img3d001.transpose()
+    img3dfull[*idf_pb(_a([0, 128,   0]), block_size)] = img3d010.transpose()
+    img3dfull[*idf_pb(_a([0, 128, 128]), block_size)] = img3d011.transpose()
+    figure(134)
+    imgshow(f_l_gamma(img3dfull.max(axis=0).T, 3.0))
+    xlabel('y')
+    ylabel('z')
+    title('full')
+    # good
+
+    figure(135)
+    imgshow(f_l_gamma(
+        imgz[*idf_pb(p_corner, (128, 256, 256))] \
+        .max(axis=0).T, 3.0))
+    xlabel('y')
+    ylabel('z')
+    title('zarr full')
+    # not a very meaning full picture
+
+    # Results above seems consistent:
+    #   The small block in zarr is in (z,y,x) order
+    #   But the indexing for zarr is in (x,y,z) order
+
 
 def WalkTreeTrial(swc_path, image_block_path):
     # get an ordered and continuous node index tree and its graph
@@ -158,18 +264,20 @@ def WalkTreeTrial(swc_path, image_block_path):
     block_loader.ImportLychnixVolume(block_lym_path)
     vol_sel = block_loader.LoadVolumeAt(p_img_center)
     print(vol_sel)
-    plt.figure(30)
+    figure(30)
     img_lym = tifffile.imread(vol_sel[0]['image_path'])
-    plt.imshow(img_lym.max(axis=0), cmap='gray', origin='lower')
-    plt.title('lym')
+    imgshow(img_lym.max(axis=0))
+    xlabel('x')
+    ylabel('y')
+    title('lym block')
 
     # substract every coor by p_img_corner to align to the image
     #p_img_center_s = p_img_center - p_img_corner
 
     #img_tangent = NormalSlice3DImage(img3d, p_img_center_s, vt[2], -u[1])
 
-    #plt.figure(20)
-    #plt.imshow(img_tangent, cmap='gray', origin='lower')
+    #figure(20)
+    #imgshow(img_tangent)
 
 def Test3dImageSlicing():
     # load the 3D image
@@ -189,10 +297,10 @@ def Test3dImageSlicing():
     #plt.ion()
     ShowThreeViews(img3d, p_center)
 
-    plt.figure(10)
+    figure(10)
     # rescale the image by min and max
     #img_normal = (img_normal - img_normal.min()) / (img_normal.max() - img_normal.min())
-    plt.imshow(img_normal, cmap='gray', origin='lower')
+    imgshow(img_normal)
     plt.show()
 
 if __name__ == '__main__':
